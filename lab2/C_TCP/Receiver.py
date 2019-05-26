63fc833d4dde940390497076425754814c3543d7
Miheel <mikaelleuf@gmail.com>
2019-05-26 17:09:26 +0200

    TCP

---
+++ lab2/C_TCP/Receiver.py
@@ -0,0 +1,63 @@
+""" TCP Receiver """
+
+import socket as s
+import time as t
+
+def create_socket(server_port):
+    """ creates a socket """
+
+    server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    server_socket.bind(('', server_port))
+    server_socket.listen(1)
+    return server_socket
+
+def err_log(err, msg):
+    """ prints out packet order error """
+
+    print("Packets out of order")
+    if err == 1:
+        print("Sequence too big, packet nr: ", msg)
+    elif err == -1:
+        print("Sequence too small, packet nr: ", msg)
+
+def main():
+    """ main """
+    #server_socket = create_socket(25565)
+
+    print("The UDP server is ready to recieve")
+    #message_test = ["10000;asdgj", "10001;asdgj", "10002;asdgj", "10003;asdgj", "10005;asdgj", \
+#"10004;asdgj", "10005;asdgj", "10007;asdgj", "10008;asdgj", "10009;asdgj"]
+    seq_nr = []
+
+    i = 0
+    server_socket = create_socket(12000)
+
+    while True:
+        conn_sock, client_address = server_socket.accept()
+
+        message = conn_sock.recv(2048).decode()
+
+        print("[", client_address, t.perf_counter(), "]", message[0:5])
+
+        seq_nr.append(int(message[0:5]))
+        if i > 0:
+            prev_seq_nr = seq_nr[0]
+
+            new_seq_nr = seq_nr[i]
+
+            prev_seq_nr = prev_seq_nr + i
+
+            #om de nyaste är större (två eller mer) än den tidigare
+            if new_seq_nr > prev_seq_nr:
+                err_log(1, i + 1)
+            #om dew nyaste är mindre an de tidagaste
+            elif new_seq_nr < prev_seq_nr:
+                err_log(-1, i + 1)
+
+        i = i + 1
+        conn_sock.close()
+
+    print(seq_nr)
+
+if __name__ == "__main__":
+    main()
---
+++ lab2/C_TCP/Sender.py
@@ -0,0 +1,87 @@
+""" TCP Sender """
+
+import socket as s
+import time as t
+import threading as td
+
+def read_file(file_name):
+    """
+    Reads the data files
+    """
+    in_file = open(file_name, "r")
+
+    data = ""
+    for lines in in_file:
+        data = data + lines
+    
+    in_file.close()
+    return data
+
+def log_file(log_data):
+    out_file = open("logfile_sleep_0.log", "w")
+    out_file.write(log_data)
+
+    out_file.close()
+
+def split_into_packets(data, data_size):
+    """
+    split data into packets
+    """
+
+    packets = [data[i:i+data_size] for i in range(0, len(data), data_size)]
+
+    return packets
+
+def send_packets(packets, c_sock):
+    """
+    send packet
+    """
+    data = split_into_packets(packets, 1300)
+    log_data = ""
+
+    seq_nr = 10000
+    separator = ";"
+    packet = 0
+    
+    t_end = t.time() + 20
+    start_time = t.time()
+
+    while t.time() < t_end and packet < len(data): #400 1000:
+        end_time = t.time()
+        payload = str(seq_nr) + separator + data[packet]
+
+        #print("[%.3f %d] %s" % (end_time - start_time, packet % 20, payload[0:6]))
+        log_data = log_data + str("[%.3f %d] %s" % (end_time - start_time, packet % len(data), payload[0:6])) + "\n"
+
+        c_sock.send(payload.encode())
+		
+        seq_nr = int(seq_nr) + 1		
+        packet = packet + 1
+        #t.sleep(0.0498) #20/sec
+        #t.sleep(0.0200) #50/sec
+        t.sleep(0)
+
+    log_file(log_data)
+
+def main():
+    """
+    main
+    """
+    data_str = read_file("data.txt")
+    server_name = '80.78.216.114'
+    server_port = 12000
+    addr_info = (server_name, server_port)
+
+    client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    
+    client_socket.connect(addr_info)
+
+    send_packets(data_str, client_socket)
+
+    client_socket.close()
+    
+    input("press a button")
+
+
+if __name__ == "__main__":
+    main()
