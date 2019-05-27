6467b6f128c2ff4e2d49ee487a0365e1f18482d1
Miheel <mikaelleuf@gmail.com>
2019-05-27 20:59:45 +0200

    cleanup

--- lab2/C_TCP/Receiver.py
+++ lab2/C_TCP/Receiver.py
@@ -2,58 +2,61 @@
 
 import socket as s
 import time as t
-import sys
 
 def create_socket(server_port):
-    """ creates a socket """
+    """creates a socket"""
 
     server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-    server_socket.bind(('localhost', server_port))
+    server_socket.bind(('', server_port))
     server_socket.listen(1)
     return server_socket
 
 def err_log(err, msg):
-    """ prints out packet order error """
+    """prints out packet order error"""
 
     print("Packets out of order")
     if err == 1:
-        print("Sequence too big, packet nr: ", msg)
+        print("Sequence too big, packet nr: ", msg) 
     elif err == -1:
         print("Sequence too small, packet nr: ", msg)
 
 def main():
-    """ main """
-	
-    seq_nr = []
+    """main"""
 
+    seq_nr = []
+    in_loop = True
     i = 0
     server_socket = create_socket(12000)
-	
+
     while True:
         print('waiting for a connection')
         conn_sock, client_address = server_socket.accept()
-		
-        while True:
+        in_loop = True
 
+        while in_loop == True:
             message = conn_sock.recv(2048).decode()
-            print("[", client_address, t.perf_counter(), "]", message[0:5])
 
-            #seq_nr.append(int(message[0:5]))
-            #if i > 0:
-            #    prev_seq_nr = seq_nr[0]
-            
-            #    new_seq_nr = seq_nr[i]
-            
-            #    prev_seq_nr = prev_seq_nr + i
-            
-                #om de nyaste är större (två eller mer) än den tidigare
-            #    if new_seq_nr > prev_seq_nr:
-            #        err_log(1, i + 1)
-                #om dew nyaste är mindre an de tidagaste
-            #    elif new_seq_nr < prev_seq_nr:
-            #        err_log(-1, i + 1)
-            
-            #i = i + 1
+            if message:
+                print("[", client_address, t.perf_counter(), "]", message[0:5])
+                seq_nr.append(int(message[0:5]))
+                
+                if i > 0:
+                    prev_seq_nr = seq_nr[0]
+                    new_seq_nr = seq_nr[i]
+                    prev_seq_nr = prev_seq_nr + i
+
+                    #om de nyaste är större (två eller mer) än den tidigare
+                    if new_seq_nr > prev_seq_nr:
+                        err_log(1, i + 1)
+
+                    #om dew nyaste är mindre an de tidagaste
+                    elif new_seq_nr < prev_seq_nr:
+                        err_log(-1, i + 1)
+
+                    i = i + 1
+            else:
+                in_loop = False
+
         conn_sock.close()
 
     print(seq_nr)
--- lab2/C_TCP/Sender.py
+++ lab2/C_TCP/Sender.py
@@ -2,12 +2,10 @@
 
 import socket as s
 import time as t
-import sys
 
 def read_file(file_name):
-    """
-    Reads the data files
-    """
+    """Reads the data files"""
+
     in_file = open(file_name, "r")
 
     data = ""
@@ -18,24 +16,21 @@ def read_file(file_name):
     return data
 
 def log_file(log_data):
-    out_file = open("logfile_sleep_0.log", "w")
+    out_file = open("logfile_t.log", "w")
     out_file.write(log_data)
 
     out_file.close()
 
 def split_into_packets(data, data_size):
-    """
-    split data into packets
-    """
+    """split data into packets"""
 
     packets = [data[i:i+data_size] for i in range(0, len(data), data_size)]
 
     return packets
 
-def send_packets(packets, c_socket, addr_info):
-    """
-    send packet
-    """
+def send_packets(packets, c_socket, s_freq = 0, s_time = 0):
+    """send packet"""
+
     data = split_into_packets(packets, 1300)
     log_data = ""
 
@@ -43,10 +38,10 @@ def send_packets(packets, c_socket, addr_info):
     separator = ";"
     packet = 0
     
-    t_end = t.time() + 20
+    t_end = t.time() + s_time
     start_time = t.time()
 
-    while t.time() < t_end or packet < 400:#len(data): #400 1000:
+    while t.time() <= t_end and packet < len(data): #400 1000:
 
         end_time = t.time()
         payload = str(seq_nr) + separator + data[packet]
@@ -59,23 +54,28 @@ def send_packets(packets, c_socket, addr_info):
         seq_nr = int(seq_nr) + 1		
         packet = packet + 1
 		
-        t.sleep(0.0498) #20/sec
-        #t.sleep(0.0200) #50/sec
-        #t.sleep(0)
+        t.sleep(s_freq)
 
     log_file(log_data)
 
-def main():
-    """
-    main
-    """
-    data_str = read_file("data.txt")
-    server_name = 'localhost'
-    server_port = 12000
-    addr_info = (server_name, server_port)
+def create_socket(port, host):
+    """creates a socket"""
+
     client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)	
-    client_socket.connect(addr_info)
-    send_packets(data_str, client_socket, addr_info)
+    client_socket.connect((host, port))
+	
+    return client_socket
+
+def main():
+    """main"""
+	
+    send_freq = 0.0189 #50/sec #0.0489 20/sec
+    send_time = 20
+	
+    data_str = read_file("data.txt")
+    client_socket = create_socket(12000, 'localhost')
+	
+    send_packets(data_str, client_socket, send_freq, send_time)
 
     client_socket.close()
     
