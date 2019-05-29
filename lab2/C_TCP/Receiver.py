0cd0f479ada94d908bc04df676906cfe21d9edae
Miheel <mikaelleuf@gmail.com>
2019-05-29 14:45:29 +0200

    cleanup

--- lab2/C_TCP/Receiver.py
+++ lab2/C_TCP/Receiver.py
@@ -11,15 +11,6 @@ def create_socket(server_port):
     server_socket.listen(1)
     return server_socket
 
-def err_log(err, msg):
-    """prints out packet order error"""
-
-    print("Packets out of order")
-    if err == 1:
-        print("Sequence too big, packet nr: ", msg) 
-    elif err == -1:
-        print("Sequence too small, packet nr: ", msg)
-
 def main():
     """main"""
 
@@ -35,25 +26,10 @@ def main():
 
         while in_loop == True:
             message = conn_sock.recv(2048).decode()
-
             if message:
-                print("[", client_address, t.perf_counter(), "]", message[0:5])
-                seq_nr.append(int(message[0:5]))
-                
-                if i > 0:
-                    prev_seq_nr = seq_nr[0]
-                    new_seq_nr = seq_nr[i]
-                    prev_seq_nr = prev_seq_nr + i
+                print("[", client_address, t.perf_counter(), "] size", len(message), message[0:10])
 
-                    #om de nyaste är större (två eller mer) än den tidigare
-                    if new_seq_nr > prev_seq_nr:
-                        err_log(1, i + 1)
 
-                    #om dew nyaste är mindre an de tidagaste
-                    elif new_seq_nr < prev_seq_nr:
-                        err_log(-1, i + 1)
-
-                    i = i + 1
             else:
                 in_loop = False
 
